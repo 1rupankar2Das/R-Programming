79cbaf60e782576e73b0d6caf5a94fe2e5b3dbc5
Rupankar Das <137444750+1rupankar2Das@users.noreply.github.com>
2024-02-15 16:30:51 +0530

    Add files via upload

---
+++ Prog11.r
@@ -0,0 +1,14 @@
+#Slicing 
+a<-c(10,20,30,40)
+b<-c('book','pen','textbook','pencil_case')
+c<-c(TRUE,FALSE,TRUE,FALSE)
+d<-c(2.5,8,10,7)
+df<-data.frame(a,b,c,d)
+
+names(df)<-c('ID','items','stores','price')
+print("Extract multiple data")
+df[1,2]
+df[1:3,2]
+df[3:4,]
+df[1:3,2:4]
+df[1:3,c(2,4)]
