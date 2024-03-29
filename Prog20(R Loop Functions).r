d15fe146cd840ad50624a2886b4167a25224452d
Rupankar Das <137444750+1rupankar2Das@users.noreply.github.com>
2024-03-30 00:22:26 +0530

    Update Prog20(R Loop Functions).r

--- Prog20(R Loop Functions).r
+++ Prog20(R Loop Functions).r
@@ -5,3 +5,30 @@ print(m1)
 
 a_m1<-apply(m1,2,sum)
 print(a_m1)
+
+#lapply():- 
+strings<-list("apple","banana","cherry","date","elderberry")
+lengths_result<-lapply(strings,nchar)
+print(lengths_result)
+
+#mapply():- 
+#create two vectors
+vector1<-c(1,2,3,4,5)
+vector2<-c(2,4,1,2,10)
+mapply(max,vector1,vector2)
+
+#tapply():- 
+#find the age of youngest male and female
+data<-data.frame(name=c("Amy","Max","Ray","Kim","Sam","Eve","Bob"),
+                 age=c(24,22,21,23,20,24,21),
+                 gender=factor(c("F","M","M","F","M","F","M")))
+print(data)
+tapply(data$age,data$gender,min)
+
+#Split():-
+df<-data.frame(name=c("Krunal","Ankit","Rushabh","Dhaval","Tejas"),
+                 score=c(85,90,78,92,88),
+                 subject=c("Math","Math","History","History","Math"),
+                 grade=c("10th","11th","11th","10th","10th"))
+split_df<-split(df,df$subject)
+print(split_df)
